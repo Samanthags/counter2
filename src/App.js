3d2db060383a89354e5a33b8c1bd5a02085388ec
unknown <samanthags3644@gmail.com>
2019-10-24 20:46:58 -0400

    Added the decrement method

--- src/App.js
+++ src/App.js
@@ -5,17 +5,39 @@ class Counter extends Component{
   constructor (){
     super();
     this.state = {
-      coutn: 0,
+      count: 0,
     }
   }
+
+  /* Methods live below the constaructor and above the render method;*/
+
+ increment = () => {
+   this.setState({
+     count: this.state.count + 1,
+   })
+ }
+
+ decrement = () => {
+   this.setState({
+     count: this.state.count - 1,
+   })
+ }
+
+ clear = () => {
+   this.setState({
+     count: this.state.count -1,
+   })
+ }
+
   render(){
     return(
       <div className = "container">
         <div className = "navbar">Counter.js</div>
         <div className="counter">
-          <h1>What do we put here?</h1>
-          <button type="button">Increment</button>
-          <button type="button">Decrement</button>
+          <h1>{this.state.count}</h1>
+          <button type="button" onClick={this.increment}>Increment</button>
+          <button type="button" onClick={this.decrement}>Decrement</button>
+          <button type="button" onClick={this.clear}>Clear</button>
         </div>
       </div>
     )
